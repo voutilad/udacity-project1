c7a97630411450a568b752ccd7789cbcf1b4ee3f
Dave Voutila <voutilad@tomatillo.local>
2015-05-30 16:55:34 -0400

    Tested with more than 1 movie. Fixed generator code. Added some README.md text about running generator.py

---
+++ README.md
@@ -0,0 +1,9 @@
+FSND Project 1 - Movie Trailer Website
+====================================================================
+
+Author: Dave Voutila
+
+Usage
+-----
+Run:
+  python generate.py
--- generate.py
+++ generate.py
@@ -2,6 +2,11 @@
 import media
 import fresh_tomatoes
 
-madmax = media.Movie('Mad Max: Fury Road', 'http://t0.gstatic.com/images?q=tbn:ANd9GcQuK41mExh1Qv3kbXoxohWYGlcstOQ6zEnnNdSI2BGIKywQwgRI', 'https://www.youtube.com/watch?v=hEJnMQG9ev8')
+print 'Assembling movie data...'
+movies = []
+movies.append(media.Movie('Mad Max: Fury Road', 'http://t0.gstatic.com/images?q=tbn:ANd9GcQuK41mExh1Qv3kbXoxohWYGlcstOQ6zEnnNdSI2BGIKywQwgRI', 'https://www.youtube.com/watch?v=hEJnMQG9ev8'))
+movies.append(media.Movie('ESPN 30 for 30: Slaying the Badger', 'http://ecx.images-amazon.com/images/I/71KjRpHIpbL._SX425_.jpg', 'https://www.youtube.com/watch?v=6H7w89aZHZo'))
+print 'Movies list built:\n' + str(movies)
 
-fresh_tomatoes.open_movies_page([madmax])
+print 'Generating movies page and launching browser...'
+fresh_tomatoes.open_movies_page(movies)
